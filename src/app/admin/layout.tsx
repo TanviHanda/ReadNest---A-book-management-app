ab4462007310fdd148eec59448082a1caf632510
TanviHanda <tanvisona2012@gmail.com>
2025-09-27 21:28:47 +0530

    update: add sidebar.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
-// import "@/styles/admin.css";
+import "@/styles/admin.css";
 import Sidebar from "@/components/admin/Sidebar";
 const Layout = async ({ children }: { children: ReactNode }) => {
   const session = await auth();
